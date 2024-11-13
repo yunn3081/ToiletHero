64b4f637a08ef065552ff8ea02711faa252bba9a
Yu-Tong Shen <elvashen@gmail.com>
2024-11-13 04:10:46 -0500

    fix: make map loading faster

--- app/src/main/java/com/example/myapplication/toilethero/restroom/RestroomNearbyFragment.kt
+++ app/src/main/java/com/example/myapplication/toilethero/restroom/RestroomNearbyFragment.kt
@@ -20,11 +20,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 
 class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
 
@@ -32,7 +28,7 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var database: DatabaseReference
     private val viewModel: RestroomMapViewModel by activityViewModels()
-    private var isDataLoaded = false // Used to track if Firebase data is loaded
+    private var markers = mutableMapOf<String, Marker>() // Store markers by restroom ID
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
@@ -44,23 +40,16 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_restroom_nearby, container, false)
 
-        // Initialize the FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-
-        // Set up the map fragment
         val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        // Initialize the Firebase Database reference
-        database = FirebaseDatabase.getInstance().reference
+        database = FirebaseDatabase.getInstance().reference.child("restrooms")
 
         return view
     }
 
     override fun onResume() {
         super.onResume()
-        // Apply full screen mode when the fragment is visible
-//        setFullScreenMode()
         view?.findViewById<View>(R.id.map_fragment)?.requestLayout()
     }
 
@@ -69,22 +58,13 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
         if (this::map.isInitialized) {
             viewModel.cameraPosition = map.cameraPosition
         }
-        // Cancel full screen mode when the fragment is no longer visible
-//        cancelFullScreenMode()
-    }
-
-    private fun setFullScreenMode() {
-        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-    }
-
-    private fun cancelFullScreenMode() {
-        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.uiSettings.isCompassEnabled = false
+        map.uiSettings.isMapToolbarEnabled = false
 
-        // Check for location permissions
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -93,12 +73,10 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // Request location permissions if not granted
             requestLocationPermissions()
             return
         }
 
-        // Enable location and set up the map
         enableLocationOnMap()
     }
 
@@ -118,15 +96,10 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permissions granted, enable location on the map
-                enableLocationOnMap()
-            } else {
-                // Permissions denied, show a message to the user
-                Log.e("RestroomNearbyFragment", "Location permissions are required to use this feature.")
-            }
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            enableLocationOnMap()
+        } else {
+            Log.e("RestroomNearbyFragment", "Location permissions are required to use this feature.")
         }
     }
 
@@ -140,12 +113,9 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             map.isMyLocationEnabled = true
-
-            // Restore camera position from ViewModel if available
             viewModel.cameraPosition?.let {
                 map.moveCamera(CameraUpdateFactory.newCameraPosition(it))
             } ?: run {
-                // Get current location and move the camera
                 fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                     location?.let {
                         val currentLatLng = LatLng(it.latitude, it.longitude)
@@ -155,13 +125,7 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
                 }
             }
 
-            // Load restroom locations from Firebase only once
-            if (!isDataLoaded) {
-                loadRestroomsFromFirebase()
-                isDataLoaded = true
-            }
-
-            // Set a click listener for markers
+            map.setOnCameraIdleListener { loadRestroomsInVisibleRegion() }
             map.setOnMarkerClickListener { marker ->
                 val restroomId = marker.tag as? String
                 restroomId?.let {
@@ -170,16 +134,12 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
                 }
                 true
             }
-
-            // Save the camera position when the map moves
-            map.setOnCameraIdleListener {
-                viewModel.cameraPosition = map.cameraPosition
-            }
         }
     }
 
-    private fun loadRestroomsFromFirebase() {
-        database.child("restrooms").addListenerForSingleValueEvent(object : ValueEventListener {
+    private fun loadRestroomsInVisibleRegion() {
+        val bounds = map.projection.visibleRegion.latLngBounds
+        database.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (restroomSnapshot in snapshot.children) {
                     val gpsCoordinates = restroomSnapshot.child("gpsCoordinates").getValue(String::class.java)
@@ -195,15 +155,18 @@ class RestroomNearbyFragment : Fragment(), OnMapReadyCallback {
 
                             if (latitude != null && longitude != null) {
                                 val restroomLocation = LatLng(latitude, longitude)
-                                val icon = getBitmapDescriptorFromResource(R.drawable.restroom)
 
-                                val marker = map.addMarker(
-                                    MarkerOptions()
-                                        .position(restroomLocation)
-                                        .title("$buildingName ★ $rating")
-                                        .icon(icon)
-                                )
-                                marker?.tag = restroomId
+                                if (bounds.contains(restroomLocation) && !markers.containsKey(restroomId)) {
+                                    val icon = getBitmapDescriptorFromResource(R.drawable.restroom)
+                                    val marker = map.addMarker(
+                                        MarkerOptions()
+                                            .position(restroomLocation)
+                                            .title("$buildingName ★ $rating")
+                                            .icon(icon)
+                                    )
+                                    marker?.tag = restroomId
+                                    markers[restroomId] = marker!!
+                                }
                             }
                         }
                     } else {
